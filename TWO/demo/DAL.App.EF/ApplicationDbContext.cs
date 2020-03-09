b8419345cc96af5d7d01b9b7b6c37f885f800bc6
Polina Khokhlacheva <polina.hohlatsova@ttu.ee>
2020-03-09 10:52:59 +0200

    - hw 2

---
+++ TWO/demo/DAL.App.EF/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.App.EF
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Family> Families { get; set; }
+        public DbSet<PersonType> PersonTypes { get; set; }
+        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ TWO/demo/Domain/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace Domain
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+
+        [MaxLength(64)]
+        public string FirstName { get; set; }
+        
+        [MaxLength(64)]
+        public string LastName { get; set; }
+
+        [MaxLength(255)]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+
+        public int FamilyId { get; set; } 
+        public Family Family { get; set; }
+        
+        public int PersonTypeId { get; set; }
+        public PersonType PersonType { get; set; }
+
+        //public ICollection<Family> Families { get; set; }
+    }
+}
