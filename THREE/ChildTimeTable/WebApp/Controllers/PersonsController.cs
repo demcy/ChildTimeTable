34ef4bf98c244c7ac870d91f2617a9ad097d43af
Polina.Khokhlacheva <polina.hohlatsova@ttu.ee>
2020-03-13 20:47:32 +0200

    - hw 2.1

---
+++ THREE/ChildTimeTable/WebApp/Controllers/PersonsController.cs
@@ -0,0 +1,172 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Domain;
+using WebApp.Data;
+
+namespace WebApp.Controllers
+{
+    public class PersonsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PersonsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Persons
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Persons.Include(p => p.Family).Include(p => p.IdentityUser).Include(p => p.PersonType);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Persons/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.Persons
+                .Include(p => p.Family)
+                .Include(p => p.IdentityUser)
+                .Include(p => p.PersonType)
+                .FirstOrDefaultAsync(m => m.PersonId == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return View(person);
+        }
+
+        // GET: Persons/Create
+        public IActionResult Create()
+        {
+            ViewData["FamilyId"] = new SelectList(_context.Families, "FamilyId", "FamilyId");
+            ViewData["IdentityUserId"] = new SelectList(_context.Users, "Id", "Id");
+            ViewData["PersonTypeId"] = new SelectList(_context.PersonTypes, "PersonTypeId", "PersonTypeId");
+            return View();
+        }
+
+        // POST: Persons/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PersonId,FirstName,LastName,IdentityUserId,FamilyId,PersonTypeId")] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(person);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["FamilyId"] = new SelectList(_context.Families, "FamilyId", "FamilyId", person.FamilyId);
+            ViewData["IdentityUserId"] = new SelectList(_context.Users, "Id", "Id", person.IdentityUserId);
+            ViewData["PersonTypeId"] = new SelectList(_context.PersonTypes, "PersonTypeId", "PersonTypeId", person.PersonTypeId);
+            return View(person);
+        }
+
+        // GET: Persons/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.Persons.FindAsync(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            ViewData["FamilyId"] = new SelectList(_context.Families, "FamilyId", "FamilyId", person.FamilyId);
+            ViewData["IdentityUserId"] = new SelectList(_context.Users, "Id", "Id", person.IdentityUserId);
+            ViewData["PersonTypeId"] = new SelectList(_context.PersonTypes, "PersonTypeId", "PersonTypeId", person.PersonTypeId);
+            return View(person);
+        }
+
+        // POST: Persons/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PersonId,FirstName,LastName,IdentityUserId,FamilyId,PersonTypeId")] Person person)
+        {
+            if (id != person.PersonId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(person);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PersonExists(person.PersonId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["FamilyId"] = new SelectList(_context.Families, "FamilyId", "FamilyId", person.FamilyId);
+            ViewData["IdentityUserId"] = new SelectList(_context.Users, "Id", "Id", person.IdentityUserId);
+            ViewData["PersonTypeId"] = new SelectList(_context.PersonTypes, "PersonTypeId", "PersonTypeId", person.PersonTypeId);
+            return View(person);
+        }
+
+        // GET: Persons/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.Persons
+                .Include(p => p.Family)
+                .Include(p => p.IdentityUser)
+                .Include(p => p.PersonType)
+                .FirstOrDefaultAsync(m => m.PersonId == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return View(person);
+        }
+
+        // POST: Persons/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var person = await _context.Persons.FindAsync(id);
+            _context.Persons.Remove(person);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PersonExists(int id)
+        {
+            return _context.Persons.Any(e => e.PersonId == id);
+        }
+    }
+}
---
+++ THREE/ChildTimeTable/WebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+
+        public DbSet<Family> Families { get; set; } = default!;
+        public DbSet<Person> Persons { get; set; } = default!;
+        public DbSet<PersonType> PersonTypes { get; set; } = default!;
+        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
